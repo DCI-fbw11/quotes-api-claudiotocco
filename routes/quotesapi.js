60aeb89818aef5819b09cb8f4a0d87fa96b95900
Claudio Tocco <tocco.a.claudio@gmail.com>
2019-05-12 12:08:49 +0200

    removing mongoose from api.js

--- routes/quotesapi.js
+++ routes/quotesapi.js
@@ -1,3 +1,37 @@
-const mongoose = require("mongoose");
+const express = require("express");
+const apiRouter = express.Router({ strict: true });
 
-mongoose.connect("mongodb://localhost/my_database", { useNewUrlParser: true });
+//Middleware
+
+//Helpers
+
+//Controller functions
+const { getBookmarks } = require("../controller/quote");
+
+//Route Config
+const apiRoutes = {
+  getAllQuotes: "/quotes",
+  getQuoteByID: "/quotes/:id",
+  postQuote: "/quotes",
+  updateQuoteById: "/quotes/:id",
+  deleteBookmarkById: "/quotes/:id",
+  batchDeleteBookmarks: "/quotes/delete/",
+  falseRoute: "/quotes/"
+};
+
+// Routes showing users possible endpoints for routes
+apiRouter.get("/", (req, res) => {
+  res.json({ availableRoutes: apiRoutes });
+});
+
+// Bad Requests Route
+
+// Get
+
+// Post
+
+// Update
+
+// Delete
+
+//Batch Delete
